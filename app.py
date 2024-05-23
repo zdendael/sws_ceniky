f029e43e80a5e618500fff327941fc098171d6e5
zdendael <lehocky@wia.cz>
2024-05-23 02:44:30 +0200

    více zón

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.secret_key = 'supersecretkey'
 # Seznamy zón pro Evropskou unii (zóna EU-F a EU-M)
 zona_EU_F = ["BEL-F", "BGR-F", "DNK-F", "EST-F", "FIN-F", "FRA-F", "HRV-F", "IRL-F", "ITA-F", "CYP-F", "LTU-F", "LVA-F", "LUX-F", "HUN-F", "MLT-F", "DEU-F", "NLD-F", "POL-F", "PRT-F", "AUT-F", "ROU-F", "GRC-F", "SVK-F", "SVN-F", "ESP-F", "SWE-F"]
 zona_EU_M = ["BEL-M", "BGR-M", "DNK-M", "EST-M", "FIN-M", "FRA-M", "HRV-M", "IRL-M", "ITA-M", "CYP-M", "LTU-M", "LVA-M", "LUX-M", "HUN-M", "MLT-M", "DEU-M", "NLD-M", "POL-M", "PRT-M", "AUT-M", "ROU-M", "GRC-M", "SVK-M", "SVN-M", "ESP-M", "SWE-M"]
+vsechny_zony = sorted(zona_EU_F + zona_EU_M)
 
 # Načtení CSV souboru do seznamu
 def load_csv(filename):
@@ -22,23 +23,30 @@ def save_csv(filename, data):
         writer.writerows(data)
 
 # Úprava hodnot v seznamu
-def modify_values(zmeny, nazev_zony, cena_hovoru, zmena_tarifikace, tarifikace_1, tarifikace_2):
-    zmenene_zony = []
+def modify_values(zmeny, zony_data):
+    for zona in zony_data:
+        nazev_zony = zona['nazev_zony']
+        cena_hovoru = zona['cena_hovoru']
+        zmena_tarifikace = zona['zmena_tarifikace']
+        tarifikace_1 = zona['tarifikace_1']
+        tarifikace_2 = zona['tarifikace_2']
+        
+        zmenene_zony = []
 
-    if nazev_zony == "EU-F":
-        zmenene_zony.extend(zona_EU_F)
-    elif nazev_zony == "EU-M":
-        zmenene_zony.extend(zona_EU_M)
-    else:
-        zmenene_zony.append(nazev_zony)
+        if nazev_zony == "EU-F":
+            zmenene_zony.extend(zona_EU_F)
+        elif nazev_zony == "EU-M":
+            zmenene_zony.extend(zona_EU_M)
+        else:
+            zmenene_zony.append(nazev_zony)
 
-    for row in zmeny:
-        if row[3] in zmenene_zony:
-            row[5] = cena_hovoru
-            row[7] = cena_hovoru
-            if zmena_tarifikace == 'a':
-                row[6] = tarifikace_1
-                row[8] = tarifikace_2
+        for row in zmeny:
+            if row[3] in zmenene_zony:
+                row[5] = cena_hovoru
+                row[7] = cena_hovoru
+                if zmena_tarifikace == 'a':
+                    row[6] = tarifikace_1
+                    row[8] = tarifikace_2
 
 @app.route('/')
 def index():
@@ -47,7 +55,7 @@ def index():
     elif not os.path.isfile('data/Default_CZK_1.csv'):
         flash('Soubor "Default_CZK_1.csv" nebyl nalezen ve složce "data". Ujistěte se, že soubor je na správném místě.')
 
-    return render_template('index.html')
+    return render_template('index.html', vsechny_zony=vsechny_zony)
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -55,20 +63,24 @@ def process():
         vstupni_soubor = 'data/Default_CZK_1.csv'
         zmeny = load_csv(vstupni_soubor)
 
-        nazev_zony = request.form['nazev_zony']
-        cena_hovoru = request.form['cena_hovoru']
-        zmena_tarifikace = request.form.get('zmena_tarifikace')
-        tarifikace_1 = request.form.get('tarifikace_1')
-        tarifikace_2 = request.form.get('tarifikace_2')
+        zony_data = []
+        for i in range(len(request.form.getlist('nazev_zony'))):
+            zony_data.append({
+                'nazev_zony': request.form.getlist('nazev_zony')[i],
+                'cena_hovoru': request.form.getlist('cena_hovoru')[i],
+                'zmena_tarifikace': request.form.getlist('zmena_tarifikace')[i],
+                'tarifikace_1': request.form.getlist('tarifikace_1')[i],
+                'tarifikace_2': request.form.getlist('tarifikace_2')[i]
+            })
 
-        modify_values(zmeny, nazev_zony, cena_hovoru, zmena_tarifikace, tarifikace_1, tarifikace_2)
+        modify_values(zmeny, zony_data)
 
         nazev_noveho_souboru = request.form['nazev_noveho_souboru']
         novy_soubor_path = f"data/{nazev_noveho_souboru}"
         save_csv(novy_soubor_path, zmeny)
 
         flash('Změny byly úspěšně provedeny. Můžete pokračovat v úpravách nebo stáhnout nový soubor.')
-        return render_template('index.html', nazev_noveho_souboru=nazev_noveho_souboru)
+        return render_template('index.html', vsechny_zony=vsechny_zony, nazev_noveho_souboru=nazev_noveho_souboru)
 
     except Exception as e:
         flash(f"Došlo k chybě: {e}")
